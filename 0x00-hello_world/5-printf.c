7b3651dbd391732cf9595c34bbef7c48bc49d2ea
iahmab <idrisabraham92@gmail.com>
2022-12-08 12:17:26 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * Return: always returns 0
+ * Return: always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,with proper grammar,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
