a28a600116330a0b280dd05bfef81061846e2bd4
iahmab <idrisabraham92@gmail.com>
2022-12-16 12:41:34 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print alphabe - a function that prints alphabet
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int n, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
