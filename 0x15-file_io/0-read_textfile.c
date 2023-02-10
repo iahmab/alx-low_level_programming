c3af2846ba6c040a37a2ebe30f2cf0dd8a3e58a0
iahmab <idrisabraham92@gmail.com>
2023-02-10 00:46:39 -0800

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,35 +3,39 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
+ * read_textfile - that reads a text file and prints
  * @filename: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
 
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int i = 0, file;
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_RDONLY);
 
 	if (file == -1)
-		return (-1);
+	{
+		free(text);
+		return (0);
+	}
 
+	let = read(file, text, letters);
 
-	write(file, text_content, i);
+	w = write(STDOUT_FILENO, text, let);
 
-	return (1);
+	close(file);
+
+	return (w);
 }
